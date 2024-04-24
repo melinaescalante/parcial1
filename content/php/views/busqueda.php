0cc033091cf9583a3a1cec980e0cded6a1688885
melina-escalante <meliescalantee@gmail.com>
2024-04-23 22:48:48 -0300

    añadimos estructura a parte de locales

--- content/php/views/busqueda.php
+++ content/php/views/busqueda.php
@@ -2,16 +2,14 @@
 function createCardbyId($name, $img,$plot,$pages,$price,$genre,$autor){
   
   ?>
-      <div class="card col-10 col-md-4 col-lg-3"><img src="<?=$img;?>" class="card-img-top" alt="<?=$name;?>">    <div class="card-body"><h5 class="card-title"><?=$name;?></h5><p class="card-text"><?=$plot;?></p>    </div> <ul class="list-group list-group-flush"> <li class="list-group-item"><?=$autor; ?></li> <li class="list-group-item"><?= ucwords($genre); ?></li><li class="list-group-item"><?=$pages;?> páginas</li><li class="list-group-item fw-semibold">$<?=$price;?> </li></ul><div class="card-body"><button type="button" class="btn btn-outline-primary">COMPRAR</button></div></div>
+      <div class="card col-8 col-md-4 col-lg-2 m-5"><img src="<?=$img;?>" class="card-img-top" alt="<?=$name;?>">    <div class="card-body"><h5 class="card-title"><?=$name;?></h5><p class="card-text"><?=$plot;?></p>    </div> <ul class="list-group list-group-flush"> <li class="list-group-item"><?=$autor; ?></li> <li class="list-group-item"><?= ucwords($genre); ?></li><li class="list-group-item"><?=$pages;?> páginas</li><li class="list-group-item fw-semibold">$<?=$price;?> </li></ul><div class="card-body"><button type="button" class="btn btn-outline-primary">COMPRAR</button></div></div>
       <?php 
-
-
 }
-
 
 function createAnError($msg){
   ?>
-  <div class="alert alert-warning" role="alert">
+  
+  <div class="alert alert-warning m-5" role="alert">
 
     <p><?=$msg;?></p>
   </div>
---
+++ content/php/views/locales.php
@@ -0,0 +1,57 @@
+
+<div class="contaner-fluid container-xxl mt-5">
+<div class="contenedorSucursal row justify-content-around mt-4 mb-4">
+    <h1 class="text-center m-4">Nuestras sucursales</h1>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+    <div class="row contenedor justify-content-center">
+        
+    <h2 class="text-center m-4">No dudes en contactarnos ante cualquier inquietud!</h2>
+    <form class="col-12 col-md-8 col-lg-6 mt-4 mb-4">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Ingrese su email:</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">Nunca lo compartiremos con nadie más.</div>
+        </div>
+        <select class="form-select" aria-label="Default select example">
+            <option selected disabled>Selecciona un tema</option>
+            <option value="1">Quiero un libro que no tienen.</option>
+            <option value="2">Inquietud de Sucursales</option>
+            <option value="3">Otro(especificar en el texto)</option>
+        </select>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Ingrese su inquietud.</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Suscribirme al newsletter</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+</div>    
+</div>
