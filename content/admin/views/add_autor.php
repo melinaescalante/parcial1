5184fd3fe52055ed580943e73971ab171d21c7d8
melina-escalante <meliescalantee@gmail.com>
2024-05-21 22:22:56 -0300

    creamos vista del añadir autor

---
+++ content/admin/views/add_autor.php
@@ -0,0 +1,14 @@
+<form class="container form-autor row d-flex flex-column align-items-center">
+    <h1 class="text-center m-4">Agregá un autor</h1>
+    <div class="form-group col-12 col-lg-4 col-md-6 mt-4">
+    <label class="mb-2" for="autor_nopmbre">Autor Nombre:</label>
+    <input type="text" class="form-control" id="autor_nopmbre" aria-describedby="emailHelp" placeholder="Nombre">
+
+  </div>
+  <div class="form-group col-12 col-lg-4 col-md-6 mt-4">
+    <label class="mb-2" for="autor_biografía">Autor Biografía:</label>
+    <input type="text" class="form-control" id="autor_biografía" placeholder="Biografía">
+  </div>
+
+  <button type="submit" class="btn btn-primary col-lg-4">Submit</button>
+</form>
