a5245398813911c4d29de32a78bc2cf8f42cb79a
melina-escalante <meliescalantee@gmail.com>
2024-07-24 21:33:07 -0300

    UI profile done

--- content/php/views/profile.php
+++ content/php/views/profile.php
@@ -4,26 +4,23 @@ $user = (new Usuario())->buscar_x_id($_SESSION['login']['id']);
 <div class="container">
     <div class="container mx-auto row my-5">
         <div class="col d-flex align-items-center flex-column">
-            <h1 class="text-center mb-5 fw-bold">Mi Perfil</h1>
+            <h1 class="text-center mb-4 fw-bold">Mi Perfil</h1>
             <p>Informacion Personal</p>
             <ul class="list-group">
-                <li class="list-group-item"><?=ucwords($user->getNombreCompleto())?></li>
-                <li class="list-group-item"><?=$user->getEmail()?></li>
-                <li class="list-group-item">Ordenes Activas:<?php 
+                <li class="list-group-item"><i style="color:rgb(89 170 157)" class="fa-solid fa-user me-2"></i><?=ucwords($user->getNombreCompleto())?></li>
+                <li class="list-group-item"><i style="color:rgb(89 170 157)" class="fa-solid fa-envelope me-2"></i><?=$user->getEmail()?></li>
+                <?php
+                ?>
+                <?php 
                 $purchases= (new Purchases())->compras_x_usuario($_SESSION['login']['id']);
                 if ($purchases) {
+                    ?><li class="list-group-item"><i  style="color:rgb(89 170 157)" class="fa-solid fa-cart-shopping me-2"></i>Ordenes Activas:<?php 
                     for ($i=0; $i < count($purchases); $i++) { 
+                        echo "<span class='ms-4' style='display:block'>#". $purchases[$i]->getOrderNumber()."</span>";
                         
-                        // $librosComprados = $purchases[$i]->getLibrosComprados();
-                        // print_r($librosComprados);
-                        // foreach ($librosComprados as $librosComprado) {
-
-                            // $libro = $librosComprado['libro'];
-                            echo "<span style='display:block'>#". $purchases[$i]->getOrderNumber()."</span>";
-                        // }
                     }
-                }
-                ?></li>
+                        ?></li><?php
+                }?>
                 
             </ul>
         </div>
